c0085f2128691199f60cdfffbc2423f9159c0560
unknown <rugcosta1234@gmail.com>
2025-02-24 14:51:03 -0300

    fetch and display now playing movies on Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,8 +2,9 @@ import { useEffect, useState } from "react";
 import api from "../../service/api";
 
 function Home() {
-  const [filmes, setFilmes] = useState();
+  const [filmes, setFilmes] = useState([]);
 
+  //Ao carregar a pagina, faz uma requisição para API
   useEffect(() => {
     async function laodFilmes() {
       const response = await api.get("movie/now_playing", {
@@ -13,14 +14,23 @@ function Home() {
           page: 1,
         },
       });
-      console.log(response);
+      console.log(response.data);
+      setFilmes(response.data.results.slice(0, 10));
     }
     laodFilmes();
   }, []);
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div>
+      <div>
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+            </article>
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
