747cc260ad7db98398c2f83a2659294173a09f27
Ruan <rugcosta1234@gmail.com>
2025-02-26 23:47:18 -0300

    add Error component and implement fallback route

--- src/route/route.jsx
+++ src/route/route.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Filme from "../pages/Filme/Filme";
 import Header from "../components/Header/Header";
+import Error from "../components/Error/Error";
 
 function RoutesApp() {
   return (
@@ -11,6 +12,8 @@ function RoutesApp() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/filme/:id" element={<Filme />} />
+
+          <Route path="*" element={<Error/>}/>
         </Routes>
       </BrowserRouter>
     </>
