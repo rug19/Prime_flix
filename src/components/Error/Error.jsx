02074c10cd82efe673ea1f504a2e03b333ff11df
Ruan <rugcosta1234@gmail.com>
2025-02-28 00:54:53 -0300

    add Error component with styling for 404 page

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,8 +1,10 @@
-export default function Error(){
-    return(
-        <div>
-            <h1>Teste</h1>
-        </div>
+import "./Error.css";
 
-    )
+export default function Error() {
+  return (
+    <div className="container">
+      <h1 className="not-found">404</h1>
+      <p className="text">Pagina não encontrada...</p>
+    </div>
+  );
 }
