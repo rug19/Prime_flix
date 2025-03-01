efd410dbbaefb8d73748f35e8f6ece7a2785e2c2
Ruan <rugcosta1234@gmail.com>
2025-02-28 23:47:50 -0300

    refactor Error component layout and styling for improved readability

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -2,9 +2,9 @@ import "./Error.css";
 
 export default function Error() {
   return (
-    <div className="container">
-      <h1 className="not-found">404</h1>
-      <p className="text">Pagina não encontrada...</p>
+    <div className="not-found">
+      <h1>404</h1>
+      <h2>Pagina não encontrada...</h2>
     </div>
   );
 }
